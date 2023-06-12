d133a697c2bc5d25bca120ab0343eb355fabe0f5
Swapnil Gupta <swapnil.gupta2805@gmail.com>
2023-06-12 14:47:47 +0530

    Twitter changes

--- src/components/PokemonFinder/PokemonFinder.js
+++ src/components/PokemonFinder/PokemonFinder.js
@@ -48,7 +48,7 @@ function PokemonFinder() {
           }
         />
         <meta name="og:image" content="/images/channels4_profile.jpg" />*/}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Pokemon Finder" />
         <meta name="twitter:site" content="@pokemonfinder"></meta>
         <meta
